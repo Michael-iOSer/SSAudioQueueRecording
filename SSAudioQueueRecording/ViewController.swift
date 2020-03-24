45ac645d28aca957acbfdd41c830f0bfd0c03905
Michael <michael.wzm@gmail.com>
2020-03-24 20:31:22 +0800

    Initial Commit

---
+++ SSAudioQueueRecording/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SSAudioQueueRecording
+//
+//  Created by Michael on 2020/3/24.
+//  Copyright © 2020 Michael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
